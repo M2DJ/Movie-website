c5f515ccca8b0553daf97ff810219bba31c904a0
M2DJ Gaming <m2dj2004@gmail.com>
2025-07-15 15:37:45 +0300

    Added css styling to the movie review component

--- src/components/MovieReview.jsx
+++ src/components/MovieReview.jsx
@@ -1,22 +1,25 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import '../css/MovieReview.css'
 
 const MovieReview = () => {
   const { state } = useLocation();
   const movie = state?.movie;
 
-  if(!movie) return <div>Movie not found</div>
+  if (!movie) return <div>Movie not found</div>;
 
   return (
     <div className="movie-review-container">
-        <div className="movie-details">
-            <img src={movie.poster_path} alt={movie.title}/>
-            <h1>{movie.title}</h1>
-        </div>
-        <div className="movie-review">
-            {movie.review}
-        </div>
+        <img
+          className="movie-review-poster"
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      <div className="movie-review">
+        <h1>{movie.title}</h1>
+        <div>{movie.review}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieReview
+export default MovieReview;
